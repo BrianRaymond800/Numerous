6b36399f86c1081207224226b3260d53936b2a0e
Bagel82 <122378674+Bagel82@users.noreply.github.com>
2024-12-06 16:28:04 -0500

    Functions are now identical, besides a variable i and which function they call next.

--- src/main/java/org/Numerous/RomanNumeralConverter.java
+++ src/main/java/org/Numerous/RomanNumeralConverter.java
@@ -12,120 +12,126 @@ public class RomanNumeralConverter {
     }
 
     private String convertLessThanFourThousand(int number, String numeral){
+        int i = 0;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 0 + 2 - (0 % 2);
-        if (number < numbers[0] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if (number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanNineHundred(number, numeral);
-        }else if(number < numbers[0]){
+        }else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[0];
-            number -= numbers[0] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         }else{
-            numeral += numerals[0];
-            number -= numbers[0];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
     }
 
     private String convertLessThanNineHundred(int number, String numeral){
+        int i = 1;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 1 + 2 - (1 % 2);
-        if(number < numbers[1] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if(number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanFourHundred(number, numeral);
-        } else if(number < numbers[1]){
+        } else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[1];
-            number -= numbers[1] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         } else{
-            numeral += numerals[1];
-            number -= numbers[1];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
     }
 
     private String convertLessThanFourHundred(int number, String numeral){
+        int i = 2;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 2 + 2 - (2 % 2);
-        if(number < numbers[2] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if(number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanNinety(number, numeral);
-        }else if(number < numbers[2]){
+        }else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[2];
-            number -= numbers[2] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         }else{
-            numeral += numerals[2];
-            number -= numbers[2];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
     }
 
     private String convertLessThanNinety(int number, String numeral){
+        int i = 3;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 3 + 2 - (3 % 2);
-        if(number < numbers[3] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if(number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanForty(number, numeral);
-        } else if(number < numbers[3]){
+        } else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[3];
-            number -= numbers[3] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         } else{
-            numeral += numerals[3];
-            number -= numbers[3];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
     }
 
     private String convertLessThanForty(int number, String numeral){
+        int i = 4;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 4 + 2 - (4 % 2);
-        if(number < numbers[4] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if(number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanNine(number, numeral);
-        } else if(number < numbers[4]){
+        } else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[4];
-            number -= numbers[4] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         } else{
-            numeral += numerals[4];
-            number -= numbers[4];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
     }
 
     private String convertLessThanNine(int number, String numeral){
+        int i = 5;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 5 + 2 - (5 % 2);
-        if(number < numbers[5] - numbers[subtractionNumber]){
+        int subtractionNumber = i + 2 - (i % 2);
+        if(number < numbers[i] - numbers[subtractionNumber]){
             numeral = convertLessThanFour(number, numeral);
-        } else if(number < numbers[5]){
+        } else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[5];
-            number -= numbers[5] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         } else{
-            numeral += numerals[5];
-            number -= numbers[5];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
@@ -133,20 +139,21 @@ public class RomanNumeralConverter {
     }
 
     private String convertLessThanFour(int number, String numeral){
+        int i = 6;
         if(number == 0){
             return "";
         }
-        int subtractionNumber = 6 + 2 - (6 % 2);
+        int subtractionNumber = i + 2 - (i % 2);
         if(number < numbers[6] - numbers[subtractionNumber]){
 
-        } else if(number < numbers[6]){
+        } else if(number < numbers[i]){
             numeral += numerals[subtractionNumber];
-            numeral += numerals[6];
-            number -= numbers[6] - numbers[subtractionNumber];
+            numeral += numerals[i];
+            number -= numbers[i] - numbers[subtractionNumber];
             numeral += convertToRomanNumeral(number);
         } else{
-            numeral += numerals[6];
-            number -= numbers[6];
+            numeral += numerals[i];
+            number -= numbers[i];
             numeral += convertToRomanNumeral(number);
         }
         return numeral;
